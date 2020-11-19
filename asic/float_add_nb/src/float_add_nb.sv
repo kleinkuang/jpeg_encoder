2e963a10b01411d51936c8b1e11d0ef944186e1c
kleinkuang <klein_kuang@163.com>
2020-11-19 20:48:47 +0000

    add ok

--- asic/float_add_nb/src/float_add_nb.sv
+++ asic/float_add_nb/src/float_add_nb.sv
@@ -4,7 +4,7 @@
 // @ Imperial College London
 
 // Non-blocking Float-Point Adder (Single-Precision)
-// - Latency: ? clock cycles
+// - Latency: 6 clock cycles
 
 // IEEE 754 Format
 // Signed + Exponent + Fraction
@@ -45,83 +45,109 @@ module float_add_nb
 );
 
 // -------------------------------------------------------------
-// Compare Exponent and Exchange
+// Input Assignment
 // -------------------------------------------------------------
-logic        coex_sig1;
-logic        coex_sig2;
-logic [7:0]  coex_exp1_int; 
-logic [7:0]  coex_exp2_int;
-logic [7:0]  coex_exp; 
-logic [23:0] coex_man1_int;
-logic [23:0] coex_man2_int;
-logic [25:0] coex_man1;
-logic [49:0] coex_man2;
-logic        coex_valid;
+logic        sig1, sig2;
+logic [7:0]  exp1, exp2;
+logic [22:0] man1, man2;
 
-logic        to_exchg; // Exchange inputs
-logic [7:0]  to_shift;
+assign {sig1, sig2} = {din1[31],    din2[31]};
+assign {exp1, exp2} = {din1[30:23], din2[30:23]};
+assign {man1, man2} = {din1[22:0],  din2[22:0]};
 
-assign to_exchg = din2[30:23] > din1[30:23];
-assign to_shift = to_exchg ? din2[30:23] - din1[30:23] : din1[30:23] - din2[30:23];
+// -------------------------------------------------------------
+// Input Exchange
+// -------------------------------------------------------------
+logic [8:0]  in_diff;
+logic        in_exchg;
 
-assign coex_exp1_int = to_exchg ? din2[30:23] : din1[30:23];
-assign coex_exp2_int = to_exchg ? din1[30:23] : din2[30:23];
+logic        in_sig;
+logic        in_sigx;
+logic [7:0]  in_exp;
+logic [23:0] in_man1;
+logic [23:0] in_man2;
+logic        in_valid;
+logic [7:0]  in_shift;
 
-assign coex_man1_int = {coex_exp1_int=='0 ? 1'b0 : 1'b1, to_exchg ? {din2[22:0]} : {din1[22:0]}};
-assign coex_man2_int = {coex_exp2_int=='0 ? 1'b0 : 1'b1, to_exchg ? {din1[22:0]} : {din2[22:0]}};
+assign in_diff  = {1'b0, exp1} - {1'b0, exp2};
+assign in_exchg = in_diff[8];
 
 always_ff @ (posedge clk, negedge nrst)
     if(~nrst)
-        coex_valid <= '0;
+        in_valid <= '0;
     else
-        coex_valid <= din_valid;
-        
+        in_valid <= din_valid;
+
 always_ff @ (posedge clk)
     if(din_valid) begin
-        coex_sig1 <= to_exchg ? din2[31] : din1[31];
-        coex_sig2 <= to_exchg ? din1[31] : din2[31];
-        coex_exp  <= coex_exp1_int;
-        coex_man1 <= {2'b0, coex_man1_int};
-        coex_man2 <= {2'b0, {coex_man2_int, 24'd0} >> to_shift};
+        in_sig   <= in_exchg ? sig2 : sig1;
+        in_sigx  <= sig1 ^ sig2;
+        in_exp   <= in_exchg ? exp2 : exp1;
+        in_man1  <= in_exchg ? {exp2!='0, man2} : {exp1!='0, man1};
+        in_man2  <= in_exchg ? {exp1!='0, man1} : {exp2!='0, man2};
+        in_shift <= in_exchg ? (exp2 - exp1) : (exp1 - exp2);
     end
 
+// -------------------------------------------------------------
+// Input Shift
+// -------------------------------------------------------------
+logic [47:0] sf_man2_int;
+logic [2:0]  sf_grs_int;
+
+logic        sf_sig;
+logic        sf_sigx;
+logic [7:0]  sf_exp;
+logic [23:0] sf_man1; // 24M
+logic [26:0] sf_man2; // 24M + 3GRS
+logic        sf_valid;
+
+assign sf_man2_int = {in_man2, 24'b0} >> (in_shift>8'd24 ? 8'd24 : in_shift);
+assign sf_grs_int  = {sf_man2_int[23:22], sf_man2_int[21:0]!='0};
+
+always_ff @ (posedge clk, negedge nrst)
+    if(~nrst)
+        sf_valid <= '0;
+    else
+        sf_valid <= in_valid;
+
+always_ff @ (posedge clk)
+    if(in_valid) begin
+        sf_sig   <= in_sig;
+        sf_sigx  <= in_sigx;
+        sf_exp   <= in_exp;
+        sf_man1  <= in_man1;
+        sf_man2  <= {sf_man2_int[47:24], sf_grs_int};
+    end
+    
 // -------------------------------------------------------------
 // Pre Addition
 // -------------------------------------------------------------
 logic        pre_sig;
-logic        pre_sigx_int;
-logic        pre_sigx; // Sign Xor
+logic        pre_sigx;
 logic [7:0]  pre_exp;
-logic [28:0] pre_augend;
-logic [28:0] pre_augend_int;
-logic [28:0] pre_addend;
-logic        pre_sticky;
+logic [28:0] pre_augend;    // 1S + 1C + 24M + 3'b0
+logic [28:0] pre_addend;    // 1S + 1C + 24M + 3GRS
 logic        pre_valid;
-
-assign pre_sticky     = coex_man2[21:0]!='0;
-assign pre_sigx_int   = coex_sig1 ^ coex_sig2;
-assign pre_augend_int = {coex_man1, 3'd0};
 
 always_ff @ (posedge clk, negedge nrst)
     if(~nrst)
         pre_valid <= '0;
     else
-        pre_valid <= coex_valid;
-        
-always_ff @ (posedge clk)
-    if(coex_valid) begin
-        pre_sig    <= coex_sig1;
-        pre_exp    <= coex_exp;
-        pre_sigx   <= pre_sigx_int;
-        pre_augend <= pre_sigx_int ? ~pre_augend_int+29'd1 : pre_augend_int;
-        pre_addend <= {coex_man2[49:24], coex_man2[23:22], pre_sticky};
-    end
+        pre_valid <= sf_valid;
 
+always_ff @ (posedge clk)
+    if(sf_valid) begin
+        pre_sig    <= sf_sig;
+        pre_sigx   <= sf_sigx;
+        pre_exp    <= sf_exp;
+        pre_augend <= {2'b0, sf_man1, 3'b0};
+        pre_addend <= sf_sigx ? (~{2'b0, sf_man2} + 29'd1) : {2'b0, sf_man2};
+    end
+    
 // -------------------------------------------------------------
 // Addition
 // -------------------------------------------------------------
 logic        add_sig;
-logic        add_sigx;
 logic [7:0]  add_exp;
 logic [28:0] add_sum;
 logic        add_valid;
@@ -135,21 +161,21 @@ always_ff @ (posedge clk, negedge nrst)
 always_ff @ (posedge clk)
     if(pre_valid) begin
         add_sig  <= pre_sig;
-        add_sigx <= pre_sigx;
         add_exp  <= pre_exp;
         add_sum  <= pre_augend + pre_addend;
     end
-
+    
 // -------------------------------------------------------------
 // Post Addition
 // -------------------------------------------------------------
-logic        post_inv;
+logic        post_inv_int;
+
 logic        post_sig;
 logic [7:0]  post_exp;
 logic [28:0] post_sum;
 logic        post_valid;
 
-assign post_inv = add_sum[28];
+assign post_inv_int = add_sum[28];
 
 always_ff @ (posedge clk, negedge nrst)
     if(~nrst)
@@ -159,50 +185,77 @@ always_ff @ (posedge clk, negedge nrst)
 
 always_ff @ (posedge clk)
     if(add_valid) begin
-        post_sig <= add_sigx ? post_inv ? add_sig : ~add_sig : add_sig;
+        post_sig <= add_sig ^ post_inv_int;
         post_exp <= add_exp;
-        post_sum <= post_inv ? (~add_sum + 29'd1) : add_sum;
+        post_sum <= post_inv_int ? (~add_sum + 29'd1) : add_sum;
     end
     
 // -------------------------------------------------------------
-// Normalization
+// Round
 // -------------------------------------------------------------
-logic [4:0]  norm_lnz;
-logic        norm_sig;
-logic [7:0]  norm_exp;
-logic [22:0] norm_man;
-logic [27:0] norm_man_int;
-logic [2:0]  norm_grs_int;
-logic        norm_zero_int;
-logic        norm_valid;
+logic [4:0]  rod_lnz;
+logic [27:0] rod_man_int;
+logic [2:0]  rod_grs_int;
+logic        rod_up_int;
+
+logic        rod_sig;
+logic        rod_zero;
+logic [7:0]  rod_exp;
+logic [22:0] rod_man;
+logic        rod_up;
+logic        rod_valid;
 
 lead_nz #
 (
-    .WIDTH(25)
+    .WIDTH(28)
 ) lead_nz_inst
 (
-    .din  (post_sum[27:3]),
-    .dout (norm_lnz)
+    .din  (post_sum[27:0]), // 1C + 24Mantissa + 3GRS
+    .dout (rod_lnz)
 );
 
-assign norm_man_int  = post_sum[27:0] << (norm_lnz + 7'd1);
-assign norm_zero_int = norm_lnz==5'd25;
-assign norm_grs_int  = post_sum[2:0];
+assign rod_man_int = post_sum[27:0] << (rod_lnz + 7'd1);
+assign rod_grs_int = {rod_man_int[4:3], rod_man_int[2:0]!='0};
+assign rod_up_int = rod_grs_int[2] & (rod_grs_int[1:0]==2'd00 ? rod_man_int[5] : 1'b1);
 
 always_ff @ (posedge clk, negedge nrst)
     if(~nrst)
-        norm_valid <= '0;
+        rod_valid <= '0;
     else
-        norm_valid <= post_valid;
+        rod_valid <= post_valid;
 
 always_ff @ (posedge clk)
     if(post_valid) begin
-        norm_sig <= norm_zero_int ? '0 : post_sig;
-        norm_exp <= norm_zero_int ? '0 : (post_exp - norm_lnz + 7'd1);
-        norm_man <= norm_zero_int ? '0 : norm_man_int[27:5];// + norm_grs_int[2];
+        rod_sig  <= post_sig;
+        rod_zero <= post_sum=='0;
+        rod_exp  <= (post_exp - rod_lnz + 7'd1);
+        rod_man  <= rod_man_int[27:5];
+        rod_up   <= rod_up_int;
     end
 
-assign dout = {norm_sig, norm_exp, norm_man[22:0]};
-assign dout_valid = norm_valid;
+// -------------------------------------------------------------
+// Normalization
+// -------------------------------------------------------------
+logic        norm_sig;
+logic [7:0]  norm_exp;
+logic [22:0] norm_man;
+logic [23:0] norm_man_int;
+
+assign norm_man_int = {1'b0, rod_man} + rod_up;
+
+always_ff @ (posedge clk, negedge nrst)
+    if(~nrst)
+        dout_valid <= '0;
+    else
+        dout_valid <= rod_valid;
+
+always_ff @ (posedge clk)
+    if(rod_valid) begin
+        norm_sig  <= rod_zero ? '0 : rod_sig;
+        norm_exp  <= rod_zero ? '0 : rod_exp + norm_man_int[23];
+        norm_man  <= rod_zero ? '0 : norm_man_int[22:0];
+    end
+
+assign dout = {norm_sig, norm_exp, norm_man};
 
 endmodule
--- asic/float_add_nb/src/float_add_nb_tb.sv
+++ asic/float_add_nb/src/float_add_nb_tb.sv
@@ -20,76 +20,27 @@ initial begin
     forever #5ns clk = ~clk;
 end
 
+// ----------------------------------------------------------------
+// Control signals               
+// ----------------------------------------------------------------
 initial begin
     nrst = '0;
+    din_valid = '0;
     
     @ (posedge clk)
     nrst <= '1;
-end
-
-/*initial begin
-    din1 = 32'b10111110101011111000000000000000; // 0.3427734375
-    din2 = 32'b10111111110110101000000000000000; // 1.70703125
     
-    din_valid = '0;
-    
-    @(posedge nrst)
-    ;
-    
-    @(posedge clk)
+    @ (posedge clk)
     din_valid <= '1;
-end*/
-
-shortreal a, b, scale;
-shortreal my_res, ip_res;
-string    check;
-
-logic error = '0;
-
-initial begin
-    scale     = 0.0001;
-    din_valid = '0;
-    
-    @ (posedge nrst)
-    ;
-    
-    forever begin
-        // Random float-point number
-        a = $urandom_range(163840000, 0) * scale - 163840000 * scale / 2;
-        b = $urandom_range(163840000, 0) * scale - 163840000 * scale / 2;
-        // Float to bits
-        din1 = $shortrealtobits(a);
-        din2 = $shortrealtobits(b);
-    
-        @(posedge clk)
-        din_valid <= '1;
-        
-        @(posedge clk)
-        din_valid <= '0;
-        
-        @(posedge dout_valid) begin
-            a =      $bitstoshortreal(din1);
-            b =      $bitstoshortreal(din2);
-            my_res = $bitstoshortreal(dout);
-            ip_res = $bitstoshortreal(dout_gold);
-            check  = dout==dout_gold ? "OK" : "ERROR";
-            
-            $write("%15.4f %15.4f %15.4f %15.4f %s\n", a, b, my_res, ip_res, check);
-            
-            if(dout!=dout_gold & dout[31:1]!=dout_gold[31:1] & (dout!=dout_gold+1) & (dout+1!=dout_gold))
-                error <= '1;
-            else
-                error <= '0;
-        end
-
-        @(posedge clk)
-        ;
-    end
 end
 
+// ----------------------------------------------------------------
 // Use Xilinx Float-point IP for Varification
-logic [31:0] dout_gold;
-logic        dout_gold_valid;
+// ----------------------------------------------------------------
+logic [31:0] dout_ip_int;
+logic        dout_ip_valid_int;
+logic [31:0] dout_ip;
+logic        dout_ip_valid;
 
 float_add_gold dut_gold
 (
@@ -97,8 +48,90 @@ float_add_gold dut_gold
     .s_axis_a_tdata       (din1),
     .s_axis_b_tvalid      (din_valid),
     .s_axis_b_tdata       (din2),
-    .m_axis_result_tvalid (dout_gold_valid),
-    .m_axis_result_tdata  (dout_gold)
+    .m_axis_result_tvalid (dout_ip_valid_int),
+    .m_axis_result_tdata  (dout_ip_int)
 );
-  
+
+shift_reg #
+(
+    .WIDTH(33),
+    .STAGE(7)
+) dout_ip_sf
+(
+    .clk  (clk),
+    .din  ({dout_ip_valid_int, dout_ip_int}),
+    .dout ({dout_ip_valid, dout_ip})
+);
+
+// ----------------------------------------------------------------
+// Input Generator
+// ----------------------------------------------------------------
+shortreal din1_real;
+shortreal din2_real;
+
+shortreal scale = 0.001;
+
+initial begin
+    din1_real = $urandom_range(255000, 0) * scale - 255000 * scale / 2;
+    din2_real = $urandom_range(255000, 0) * scale - 255000 * scale / 2;
+    din1 = $shortrealtobits(din1_real);
+    din2 = $shortrealtobits(din2_real);
+
+    forever @ (posedge clk) begin
+        if(din_valid) begin
+            din1_real = $urandom_range(255000, 0) * scale - 255000 * scale / 2;
+            din2_real = $urandom_range(255000, 0) * scale - 255000 * scale / 2;
+            din1 = $shortrealtobits(din1_real);
+            din2 = $shortrealtobits(din2_real);
+        end
+    end
+end
+
+// ----------------------------------------------------------------
+// Output Checker
+// ----------------------------------------------------------------
+logic error;
+
+assign error = (dout!=dout_ip) & (dout[31:1]!=dout_ip[31:1]) & (dout!=dout_ip+1) & (dout+1!=dout_ip) & dout_valid;
+
+// Print
+string check;
+integer cnt;
+
+logic [31:0] din1_sf, din2_sf;
+
+shift_reg #
+(
+    .WIDTH(64),
+    .STAGE(7)
+) din_sf
+(
+    .clk  (clk),
+    .din  ({din1, din2}),
+    .dout ({din1_sf, din2_sf})
+);
+
+initial begin
+
+    $write("%15s %15s %15s %15s %15s %10s\n", "No. Test", "Multiplicand", "Multiplier", "IP Out", "My Out", "Check");
+    cnt = 1;
+    
+    forever @ (posedge clk) begin
+        if(dout_valid) begin
+        
+            if(dout=='0)
+                check = dout[30:0]==dout_ip[30:0] ? "OK" : "ERROR";
+            else
+                check = dout==dout_ip ? "OK" : "ERROR";
+            
+            $write("%15d %15.4f %15.4f %15.4f %15.4f %10s\n", cnt,
+                                                              $bitstoshortreal(din1_sf), $bitstoshortreal(din2_sf),
+                                                              $bitstoshortreal(dout_ip), $bitstoshortreal(dout),
+                                                              check);
+
+            cnt = cnt + 1;
+        end
+    end
+end
+
 endmodule

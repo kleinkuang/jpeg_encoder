9fd93b68bbe9c687ba70bda6bba6953eefa674b8
kleinkuang <klein_kuang@163.com>
2020-11-19 23:50:46 +0000

    div ok

--- asic/float_div_nb/src/float_div_nb.sv
+++ asic/float_div_nb/src/float_div_nb.sv
@@ -4,10 +4,10 @@
 // @ Imperial College London
 
 // Non-blocking Float-Point Divider (Single-Precision)
-// - latency: ? clock cycles
+// - latency: 28 clock cycles
 
 // IEEE 754 Format
-// Signed + Exponent + Fraction
+// signed + exponent + mantissa
 // 1b     + 8b       + 23b
 
 // Normalized
@@ -21,7 +21,7 @@
 //            frac ~= '0: NaN
 
 // Round
-// round to zero
+// - round to zero
 
 // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 // Current design does not support the following:
@@ -55,8 +55,8 @@ assign {sig1, sig2} = {din1[31]   , din2[31]};
 assign {exp1, exp2} = {din1[30:23], din2[30:23]};
 assign {man1, man2} = {din1[22:0],  din2[22:0]};
 
-logic [24:0] div_end;
-logic [24:0] div_sor;
+logic [23:0] div_end;
+logic [23:0] div_sor;
 logic        div_valid;
 
 always_ff @ (posedge clk, negedge nrst)
@@ -67,31 +67,31 @@ always_ff @ (posedge clk, negedge nrst)
 
 always_ff @ (posedge clk)
     if(din_valid) begin
-        div_end <= {1'b0, exp1=='0 ? 1'b0 : 1'b1, man1};
-        div_sor <= {1'b0, exp2=='0 ? 1'b0 : 1'b1, man2};
+        div_end <= {exp1!='0, man1};
+        div_sor <= {exp2!='0, man2};
     end
-
+    
 // -------------------------------------------------------------
 // Sign & Exponent
 // -------------------------------------------------------------
 logic       sig_int;
-logic [9:0] exp_int;
+logic [8:0] exp_int;
 
 shift_reg #
 (
-    .WIDTH(10),
-    .STAGE(27)
+    .WIDTH(9),
+    .STAGE(26)
 ) exp_sf
 (
     .clk  (clk),
-    .din  ({2'b0, exp1} - {2'b0, exp2} + 8'd127),
+    .din  ({1'b0, exp1} - {1'b0, exp2} + 9'd127),
     .dout (exp_int)
 );
 
 shift_reg #
 (
     .WIDTH(1),
-    .STAGE(27)
+    .STAGE(26)
 ) sig_sf
 (
     .clk  (clk),
@@ -102,8 +102,8 @@ shift_reg #
 // -------------------------------------------------------------
 // Long Division based Unsigned Divider
 // -------------------------------------------------------------
-logic [24:0] quo;
-logic        quo_valid;
+logic [24:0] fix_quo;
+logic        fix_valid;
 
 fixed_div_nb #
 (
@@ -113,51 +113,156 @@ fixed_div_nb #
 (
     .clk        (clk),
     .nrst       (nrst),
-    .dividend   (div_end),
-    .divisor    (div_sor),
+    .dividend   ({1'b0, div_end}),  // 1S + 24Mantissa
+    .divisor    ({1'b0, div_sor}),
     .din_valid  (div_valid),
-    .quotient   (quo),
-    .dout_valid (quo_valid)
+    .quotient   (fix_quo),
+    .dout_valid (fix_valid)
 );
 
 // -------------------------------------------------------------
 // Normalization
 // -------------------------------------------------------------
-logic [4:0]  norm_lnz;
+logic [24:0] norm_fix;
+logic        norm_zero;
+
 logic        norm_sig;
 logic [7:0]  norm_exp;
 logic [22:0] norm_man;
-logic [27:0] norm_man_int;
-//logic [2:0]  norm_grs_int;
-logic        norm_zero_int;
-logic        norm_valid;
 
-lead_nz #
-(
-    .WIDTH(25)
-) lead_nz_inst
-(
-    .din  (quo),
-    .dout (norm_lnz)
-);
-
-assign norm_man_int  = quo << (norm_lnz + 7'd1);
-assign norm_zero_int = quo=='0;
+assign norm_zero = fix_quo=='0;
+assign norm_fix  = fix_quo << (fix_quo[24] ? 2'd1 : 2'd2);
 
 always_ff @ (posedge clk, negedge nrst)
     if(~nrst)
-        norm_valid <= '0;
+        dout_valid <= '0;
     else
-        norm_valid <= quo_valid;
+        dout_valid <= fix_valid;
 
 always_ff @ (posedge clk)
-    if(quo_valid) begin
-        norm_sig <= norm_zero_int ? '0 : sig_int;
-        norm_exp <= norm_zero_int ? '0 : (exp_int - norm_lnz);
-        norm_man <= norm_zero_int ? '0 : norm_man_int[24:2];// + norm_grs_int[2];
+    if(fix_valid) begin
+        norm_sig  <= norm_zero ? '0 : sig_int;
+        norm_exp  <= norm_zero ? '0 : (exp_int[7:0] - 8'd1 + fix_quo[24]);
+        norm_man  <= norm_zero ? '0 : norm_fix[24:2];
     end
 
-assign dout = {norm_sig, norm_exp, norm_man[22:0]};
-assign dout_valid = norm_valid;
+assign dout = {norm_sig, norm_exp, norm_man};
+
+/*
+// -------------------------------------------------------------
+// Round to the nearest even version
+// -------------------------------------------------------------
+parameter PRE = 29; // Division Precision
+// Control the division precision, mainly for purpose of rounding
+// Min: 26 | Max: 48
+// - Xilinx IP is based on rounding to the nearest even
+// - !!! When PRE = 48, rounding to the nearest even can be achieved
+//   !!! however, it utlizes significant resources
+//   !!! which is almost doubled as when PRE = 27 for just 1 bit...
+// -------------------------------------------------------------
+// Sign & Exponent
+// -------------------------------------------------------------
+logic       sig_int;
+logic [8:0] exp_int;
+
+shift_reg #
+(
+    .WIDTH(9),
+    .STAGE(PRE+2)
+) exp_sf
+(
+    .clk  (clk),
+    .din  ({1'b0, exp1} - {1'b0, exp2} + 9'd127),
+    .dout (exp_int)
+);
+
+shift_reg #
+(
+    .WIDTH(1),
+    .STAGE(PRE+3)
+) sig_sf
+(
+    .clk  (clk),
+    .din  (sig1 ^ sig2),
+    .dout (sig_int)
+);
+
+// -------------------------------------------------------------
+// Long Division based Unsigned Divider
+// -------------------------------------------------------------
+logic [PRE-1:0] fix_quo;
+logic        fix_valid;
+
+fixed_div_nb #
+(
+    .WIDTH(25),
+    .STEPS(PRE)
+) div
+(
+    .clk        (clk),
+    .nrst       (nrst),
+    .dividend   ({1'b0, div_end}),  // 1S + 24Mantissa
+    .divisor    ({1'b0, div_sor}),
+    .din_valid  (div_valid),
+    .quotient   (fix_quo),
+    .dout_valid (fix_valid)
+);
+
+// -------------------------------------------------------------
+// Round
+// -------------------------------------------------------------
+logic [PRE-1:0] rod_fix;
+logic [2:0]  rod_grs;
+logic        rod_up_int;
+
+logic [22:0] rod_man;
+logic [7:0]  rod_exp;
+logic        rod_zero;
+logic        rod_up;
+logic        rod_valid;
+
+assign rod_fix    = fix_quo << (fix_quo[PRE-1] ? 2'd1 : 2'd2);
+assign rod_grs    = {rod_fix[PRE-24:PRE-25], rod_fix[PRE-26:0]!='0};
+assign rod_up_int = rod_grs[2] & (rod_grs[1:0]==2'd00 ? rod_fix[4] : 1'b1);
+
+always_ff @ (posedge clk, negedge nrst)
+    if(~nrst)
+        rod_valid <= '0;
+    else
+        rod_valid <= fix_valid;
+
+always_ff @ (posedge clk)
+    if(fix_valid) begin
+        rod_man  <= rod_fix[PRE-1:PRE-23];
+        rod_exp  <= fix_quo[PRE-1] ? exp_int[7:0] : (exp_int[7:0] - 8'd1);
+        rod_zero <= fix_quo=='0;
+        rod_up   <= rod_up_int;
+    end
+
+// -------------------------------------------------------------
+// Normalization
+// -------------------------------------------------------------
+logic        norm_sig;
+logic [7:0]  norm_exp;
+logic [22:0] norm_man;
+logic [23:0] norm_man_int;
+
+always_ff @ (posedge clk, negedge nrst)
+    if(~nrst)
+        dout_valid <= '0;
+    else
+        dout_valid <= rod_valid;
+        
+assign norm_man_int = {1'b0, rod_man} + rod_up;
+
+always_ff @ (posedge clk)
+    if(rod_valid) begin
+        norm_sig  <= rod_zero ? '0 : sig_int;
+        norm_exp  <= rod_zero ? '0 : rod_exp + norm_man_int[23];
+        norm_man  <= rod_zero ? '0 : norm_man_int[22:0];
+    end
+
+assign dout = {norm_sig, norm_exp, norm_man};
+*/
 
 endmodule
--- asic/float_div_nb/src/float_div_nb_tb.sv
+++ asic/float_div_nb/src/float_div_nb_tb.sv
@@ -1,6 +1,6 @@
 // File:    float_div_nb_tb.sv
 // Author:  Lei Kuang
-// Date:    17th November 2020
+// Date:    13th November 2020
 // @ Imperial College London
 
 module float_div_nb_tb;
@@ -20,64 +20,27 @@ initial begin
     forever #5ns clk = ~clk;
 end
 
+// ----------------------------------------------------------------
+// Control signals               
+// ----------------------------------------------------------------
 initial begin
     nrst = '0;
+    din_valid = '0;
     
     @ (posedge clk)
     nrst <= '1;
+    
+    @ (posedge clk)
+    din_valid <= '1;
 end
 
-shortreal a, b, scale;
-shortreal my_res, ip_res;
-string    check;
-
-logic error = '0;
-
-initial begin
-    scale     = 0.0001;
-    din_valid = '0;
-    
-    @ (posedge nrst)
-    ;
-    
-    forever begin
-        // Random float-point number
-        a = $urandom_range(163840000, 0) * scale - 163840000 * scale / 2;
-        b = $urandom_range(163840000, 0) * scale - 163840000 * scale / 2;
-        // Float to bits
-        din1 = $shortrealtobits(a);
-        din2 = $shortrealtobits(b);
-    
-        @(posedge clk)
-        din_valid <= '1;
-        
-        @(posedge clk)
-        din_valid <= '0;
-        
-        @(posedge dout_valid) begin
-            a =      $bitstoshortreal(din1);
-            b =      $bitstoshortreal(din2);
-            my_res = $bitstoshortreal(dout);
-            ip_res = $bitstoshortreal(dout_gold);
-            check  = dout==dout_gold ? "OK" : "ERROR";
-            
-            $write("%15.4f %15.4f %15.4f %15.4f %s\n", a, b, my_res, ip_res, check);
-            
-            if(dout!=dout_gold & dout[31:1]!=dout_gold[31:1] & (dout!=dout_gold+1) & (dout+1!=dout_gold))
-                error <= '1;
-            else
-                error <= '0;
-        end
-        
-        @(posedge clk)
-        ;
-    end
-
-end
-
+// ----------------------------------------------------------------
 // Use Xilinx Float-point IP for Varification
-logic [31:0] dout_gold;
-logic        dout_gold_valid;
+// ----------------------------------------------------------------
+logic [31:0] dout_ip_int;
+logic        dout_ip_valid_int;
+logic [31:0] dout_ip;
+logic        dout_ip_valid;
 
 float_div_gold dut_gold
 (
@@ -85,8 +48,87 @@ float_div_gold dut_gold
     .s_axis_a_tdata       (din1),
     .s_axis_b_tvalid      (din_valid),
     .s_axis_b_tdata       (din2),
-    .m_axis_result_tvalid (dout_gold_valid),
-    .m_axis_result_tdata  (dout_gold)
+    .m_axis_result_tvalid (dout_ip_valid_int),
+    .m_axis_result_tdata  (dout_ip_int)
 );
-  
+
+shift_reg #
+(
+    .WIDTH(33),
+    .STAGE(27)
+) dout_ip_sf
+(
+    .clk  (clk),
+    .din  ({dout_ip_valid_int, dout_ip_int}),
+    .dout ({dout_ip_valid, dout_ip})
+);
+
+// ----------------------------------------------------------------
+// Input Generator
+// ----------------------------------------------------------------
+shortreal din1_real;
+shortreal din2_real;
+
+shortreal scale = 0.001;
+
+initial begin
+    din1_real = $urandom_range(2047000, 0) * scale - 2047000 * scale / 2;
+    din2_real = $urandom_range(255, 1);
+    din1 = $shortrealtobits(din1_real);
+    din2 = $shortrealtobits(din2_real);
+
+    forever @ (posedge clk) begin
+        if(din_valid) begin
+            din1_real = $urandom_range(2047000, 0) * scale - 2047000 * scale / 2;
+            din2_real = $urandom_range(255, 1);
+            din1 = $shortrealtobits(din1_real);
+            din2 = $shortrealtobits(din2_real);
+        end
+    end
+end
+
+// ----------------------------------------------------------------
+// Output Checker
+// ----------------------------------------------------------------
+logic error;
+
+assign error = (dout!=dout_ip) & ((dout+1)!=dout_ip) & dout_valid;
+
+// Print
+string check;
+integer cnt;
+
+logic [31:0] din1_sf, din2_sf;
+
+shift_reg #
+(
+    .WIDTH(64),
+    .STAGE(27)
+) din_sf
+(
+    .clk  (clk),
+    .din  ({din1, din2}),
+    .dout ({din1_sf, din2_sf})
+);
+
+initial begin
+
+    $write("%15s %15s %15s %15s %15s %10s\n", "No. Test", "Multiplicand", "Multiplier", "IP Out", "My Out", "Check");
+    cnt = 1;
+    
+    forever @ (posedge clk) begin
+        if(dout_valid) begin
+        
+            check = ((dout==dout_ip) | ((dout+1)==dout_ip)) ? "OK" : "ERROR";
+            
+            $write("%15d %15.4f %15.4f %15.4f %15.4f %10s\n", cnt,
+                                                              $bitstoshortreal(din1_sf), $bitstoshortreal(din2_sf),
+                                                              $bitstoshortreal(dout_ip), $bitstoshortreal(dout),
+                                                              check);
+
+            cnt = cnt + 1;
+        end
+    end
+end
+
 endmodule
